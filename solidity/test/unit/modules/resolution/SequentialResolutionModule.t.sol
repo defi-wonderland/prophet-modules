b1934a373dd05f701e809bf54d850b961f7218fa
Gas One Cent <86567384+gas1cent@users.noreply.github.com>
2023-09-29 21:07:02 +0400

    fix: missing import

--- solidity/test/unit/modules/resolution/SequentialResolutionModule.t.sol
+++ solidity/test/unit/modules/resolution/SequentialResolutionModule.t.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.19;
 
 import 'forge-std/Test.sol';
 
-import {IOracle} from 'prophet-core-contracts/interfaces/IOracle.sol';
-import {Module, IModule} from 'prophet-core-contracts/contracts/Module.sol';
+import {IOracle} from '@defi-wonderland/prophet-core-abi/contracts/IOracle.sol';
+import {Module, IModule} from '@defi-wonderland/prophet-core-contracts/Module.sol';
 
 import {
   SequentialResolutionModule,
