ef0def005ddfe7cf3c6bce0c8e375c5b05d1e7de
0xJabberwock <0xjabberwock@defi.sucks>
2024-09-02 15:00:16 -0300

    fix: inherit from Validator interface (#59)

--- solidity/contracts/extensions/AccountingExtension.sol
+++ solidity/contracts/extensions/AccountingExtension.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {Validator} from '@defi-wonderland/prophet-core/solidity/contracts/Validator.sol';
 import {IOracle} from '@defi-wonderland/prophet-core/solidity/interfaces/IOracle.sol';
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import {EnumerableSet} from '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
-
-import {Validator} from '@defi-wonderland/prophet-core/solidity/contracts/Validator.sol';
 
 import {IAccountingExtension} from '../../interfaces/extensions/IAccountingExtension.sol';
 
