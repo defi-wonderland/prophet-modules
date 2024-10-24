b3731d349a4e6989095157e4d0d81762078dc1ed
zorzal <zorzal@defi.sucks>
2024-10-24 08:41:46 -0400

    feat: update typehashes

--- solidity/contracts/utils/Typehash.sol
+++ solidity/contracts/utils/Typehash.sol
@@ -1,18 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-bytes32 constant _PLEDGE_FOR_DISPUTE_TYPEHASH = keccak256(
-  'pledgeForDispute(IOracle.Request _request, IOracle.Dispute _dispute,IAccessController.AccessControl _accessControl)'
+bytes32 constant _PLEDGE_FOR_DISPUTE_TYPEHASH =
+  keccak256('pledgeForDispute(Request _request, Dispute _dispute,IAccessController.AccessControl _accessControl)');
+
+bytes32 constant _PLEDGE_AGAINST_DISPUTE_TYPEHASH =
+  keccak256('pledgeAgainstDispute(Request _request,Dispute _dispute,IAccessController.AccessControl _accessControl)');
+
+bytes32 constant _CLAIM_PLEDGE_TYPEHASH =
+  keccak256('claimPledge(Request _request,Dispute _dispute,AccessControl _accessControl)');
+
+bytes32 constant _CLAIM_VOTE_TYPEHASH =
+  keccak256('claimVote(Request _request,Dispute _dispute,AccessControl _accessControl)');
+
+bytes32 constant _CAST_VOTE_TYPEHASH =
+  keccak256('castVote(Request _request,Dispute _dispute,uint256 _numberOfVotes,AccessControl _accessControl)');
+bytes32 constant _COMMIT_VOTE_TYPEHASH =
+  keccak256('commitVote(Request _request,Dispute _dispute,bytes32 _commitment,AccessControl _accessControl)');
+bytes32 constant _REVEAL_VOTE_TYPEHASH = keccak256(
+  'revealVote(Request _request,Dispute _dispute,uint256 _numberOfVotes,bytes32 _salt,AccessControl _accessControl)'
 );
-
-bytes32 constant _PLEDGE_AGAINST_DISPUTE_TYPEHASH = keccak256(
-  'pledgeAgainstDispute(IOracle.Request _request, IOracle.Dispute _dispute,IAccessController.AccessControl _accessControl)'
-);
-
-bytes32 constant _CLAIM_VOTE_TYPEHASH = keccak256('');
-bytes32 constant _CLAIM_PLEDGE_TYPEHASH = keccak256('');
-
-bytes32 constant _RELEASE_UNUTILIZED_RESPONSE_TYPEHASH = keccak256('');
-bytes32 constant _CAST_VOTE_TYPEHASH = keccak256('');
-bytes32 constant _COMMIT_VOTE_TYPEHASH = keccak256('');
-bytes32 constant _REVEAL_VOTE_TYPEHASH = keccak256('');
